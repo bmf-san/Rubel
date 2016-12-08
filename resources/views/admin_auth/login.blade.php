2bda7cdcf4c2d60b26c9934370b356ccf99eeca2
bmf <bmf.infomation@gmail.com>
2016-12-08 18:50:18 +0900

    [update]

--- resources/views/admin_auth/login.blade.php
+++ resources/views/admin_auth/login.blade.php
@@ -1,13 +1,10 @@
-@extends('layouts.app')
-
-@section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
                 <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('admin/login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
@@ -65,4 +62,3 @@
         </div>
     </div>
 </div>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,6 @@
 |--------------------------------------------------------------------------
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('admin/login', 'AdminAuth\LoginController@showLoginForm');
 Route::post('admin/login', 'AdminAuth\LoginController@login');
 Route::post('admin/logout', 'AdminAuth\LoginController@logout');
-
-Route::get('/home', 'HomeController@index');
