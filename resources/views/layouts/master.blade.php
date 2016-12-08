c2b8909bfa076b7adcd1df82784971216b6448d5
bmf <bmf.infomation@gmail.com>
2016-12-08 18:56:31 +0900

    [update]

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel React Redux Blog Boilerplate</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
