a5ccbd1030852e37eaf814332356ac9529ea9ec1
bmf <bmf.infomation@gmail.com>
2016-12-08 18:52:08 +0900

    [update]

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 |--------------------------------------------------------------------------
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
