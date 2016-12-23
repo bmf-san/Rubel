096082c5f17ed1eab84a255ea6905829828827e5
bmf <bmf.infomation@gmail.com>
2016-12-24 02:04:47 +0900

    [update]

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,6 @@ Route::group(['middleware' => 'guest'], function () {
 
     Route::get('/post/{id}', 'Api\v1\Post\Postcontroller@getPost');
     Route::get('/posts', 'Api\v1\Post\PostController@getPosts');
+
+    // Post method Routing related to create, edit, update, delete posts.
 });
