074dabe232758bfde7d93b64601426f391faad99
bmf <bmf.infomation@gmail.com>
2017-01-03 03:50:05 +0900

    [update] update dashboard blade file.

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,11 +1,22 @@
-Here is admin page.
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	Here is admin page.
 
-You can do ....
+	You can do ....
 
-・create a new post
-・edit a post
-→ edit page
+	・create a new post
+	・edit a post
+	→ edit page
 
-・delete a post
-・update a post - switch publication setting of post
-→ do action in edit page or list in dashboard
+	・delete a post
+	・update a post - switch publication setting of post
+	→ do action in edit page or list in dashboard
+
+	<script type="text/javascript" src="#"></script>
+</body>
+</html>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <title>Laravel React Redux Blog Boilerplate</title>
