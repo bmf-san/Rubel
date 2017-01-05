3db961f67db5d4ba62f8222d04a3c46ad3e59c4d
bmf <bmf.infomation@gmail.com>
2017-01-05 12:14:42 +0900

    [update]

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,7 @@
 const elixir = require('laravel-elixir');
-
-require('laravel-elixir-vue-2');
-
 elixir.config.sourcemaps = false;
 
 elixir((mix) => {
     mix
-        .webpack('admin/main.js', 'public/js/admin/main.js')
+        // .webpack('./resources/assets/js/admin/main.js', 'public/js/admin/main.js')
 });
--- resources/assets/js/admin/components/Home.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>home</h1>
-</template>
