fdf9b14253c7abaf07693238acdd30432327f985
bmf <bmf.infomation@gmail.com>
2017-01-21 02:48:03 +0900

    [update] add tag autocomplete

--- resources/assets/js/admin/components/NewPost.js
+++ resources/assets/js/admin/components/NewPost.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import marked from 'marked';
 import hljs from 'highlight.js';
+import ReactTags from 'react-tag-autocomplete';
 
 export default class NewPost extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      markdown: ''
+      markdown: '',
+      tags: [
+        { id: 1, name: "Apples" },
+        { id: 2, name: "Pears" }
+      ],
+      suggestions: [
+        { id: 3, name: "Bananas" },
+        { id: 4, name: "Mangos" },
+        { id: 5, name: "Lemons" },
+        { id: 6, name: "Apricots" }
+      ]
     }
 
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleAddition = this.handleAddition.bind(this);
     this.updateMarkdown = this.updateMarkdown.bind(this);
+  }
+
+  handleDelete(i) {
+    const tags = this.state.tags
+    tags.splice(i, 1)
+    this.setState({ tags: tags })
+  }
+
+  handleAddition(tag) {
+    const tags = this.state.tags
+    tags.push(tag)
+    this.setState({ tags: tags })
   }
 
   updateMarkdown(markdown) {
@@ -23,7 +48,12 @@ export default class NewPost extends React.Component {
     return (
       <div>
         <h1>NewPost</h1>
-        <p>Here is tag select area...</p>
+        <ReactTags
+          tags={this.state.tags}
+          suggestions={this.state.suggestions}
+          handleDelete={this.handleDelete}
+          handleAddition={this.handleAddition}
+          allowNew={true} />
         <TextInput onChange={this.updateMarkdown} />
         <Markdown markdown={this.state.markdown} />
       </div>
