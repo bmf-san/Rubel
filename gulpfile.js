15b0c5740885afa3e09f19839d03f3f4630f59ea
bmf <bmf.infomation@gmail.com>
2017-01-21 01:55:01 +0900

    [update] add markdown editor

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,13 @@ elixir.config.sourcemaps = false;
 
 elixir((mix) => {
   mix
+    //-------------------------------
+    // CSS
+    //-------------------------------
+    .styles('./node_modules/highlight.js/styles/default.css', 'public/css/admin/monokai.min.css')
+
+    //-------------------------------
+    // JavaScript
+    //-------------------------------
     .webpack('./resources/assets/js/admin/index.js', 'public/js/admin/index.js')
 });
--- resources/assets/js/admin/components/NewPost.js
+++ resources/assets/js/admin/components/NewPost.js
@@ -1,12 +1,71 @@
 import React from 'react';
+import marked from 'marked';
+import hljs from 'highlight.js';
 
 export default class NewPost extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      markdown: ''
+    }
+
+    this.updateMarkdown = this.updateMarkdown.bind(this);
+  }
+
+  updateMarkdown(markdown) {
+    this.setState({
+      markdown: markdown
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>NewPost</h1>
-        <p>Here is Markdown Editor...</p>
+        <TextInput onChange={this.updateMarkdown} />
+        <Markdown markdown={this.state.markdown} />
       </div>
+    )
+  }
+}
+
+class TextInput extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this._onChange = this._onChange.bind(this);
+    }
+
+    _onChange(event) {
+      this.props.onChange(event.target.value);
+    }
+
+    render() {
+      return (
+        <textarea onChange={this._onChange}></textarea>
+      )
+    }
+}
+
+class Markdown extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidUpdate() {
+    marked.setOptions({
+      hljs: function(code, lang) {
+        return hljs.highlightAuto(code, [lang]).value;
+      }
+    });
+  }
+
+  render() {
+    var html = marked(this.props.markdown);
+
+    return (
+      <div dangerouslySetInnerHTML={{__html: html}}></div>
     )
   }
 }
