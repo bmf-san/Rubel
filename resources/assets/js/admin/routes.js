ba7fd5c3ae9ba0222d18a1f5e75fe8edc564bb9a
bmf <bmf.infomation@gmail.com>
2017-02-21 00:42:36 +0900

    [update ]

--- resources/assets/js/admin/routes.js
+++ resources/assets/js/admin/routes.js
@@ -3,20 +3,12 @@ import ReactDOM from 'react-dom';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
-import Home from './components/Home';
-import NewPost from './components/NewPost';
+import Home from './containers/Home';
 import Posts from './components/Posts';
-import PostEdit from './components/PostEdit';
-import Categories from './components/Categories';
-import Config from './components/Config';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
-    <Route path="/new-post" component={NewPost} />
     <Route path="/posts" component={Posts} />
-    <Route path="/post/edit/:post-id" component={PostEdit} />
-    <Route path="/categories" component={Categories} />
-    <Route path="/config" component={Config} />
   </Route>
 );
