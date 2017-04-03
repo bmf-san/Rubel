0da2e7c491791262b4c20012634587324d3fb553
bmf <bmf.infomation@gmail.com>
2017-04-04 02:56:08 +0900

    [update]

--- backend-app/vendor/phpunit/phpunit/tests/Framework/TestCaseTest.php
+++ backend-app/vendor/phpunit/phpunit/tests/Framework/TestCaseTest.php
@@ -29,7 +29,7 @@ class Framework_TestCaseTest extends PHPUnit_Framework_TestCase
     /**
      * Used be testStaticAttributesBackupPre
      */
-    protected static $_testStatic = 0;
+    protected static $testStatic = 0;
 
     public function testCaseToString()
     {
@@ -378,7 +378,7 @@ class Framework_TestCaseTest extends PHPUnit_Framework_TestCase
     public function testStaticAttributesBackupPre()
     {
         $GLOBALS['singleton'] = Singleton::getInstance();
-        self::$_testStatic    = 123;
+        self::$testStatic     = 123;
     }
 
     /**
@@ -387,7 +387,7 @@ class Framework_TestCaseTest extends PHPUnit_Framework_TestCase
     public function testStaticAttributesBackupPost()
     {
         $this->assertNotSame($GLOBALS['singleton'], Singleton::getInstance());
-        $this->assertSame(0, self::$_testStatic);
+        $this->assertSame(0, self::$testStatic);
     }
 
     public function testIsInIsolationReturnsFalse()
@@ -645,5 +645,45 @@ class Framework_TestCaseTest extends PHPUnit_Framework_TestCase
 
         $this->assertFalse($mock->foo());
         $this->assertNull($mock->bar());
+    }
+
+    public function testProvidingOfAutoreferencedArray()
+    {
+        $test = new \TestAutoreferenced('testJsonEncodeException', $this->getAutoreferencedArray());
+        $test->runBare();
+
+        $this->assertInternalType('array', $test->myTestData);
+        $this->assertArrayHasKey('data', $test->myTestData);
+        $this->assertEquals($test->myTestData['data'][0], $test->myTestData['data']);
+    }
+
+    /**
+     * @return array
+     */
+    private function getAutoreferencedArray()
+    {
+        $recursionData   = [];
+        $recursionData[] = &$recursionData;
+
+        return [
+            'RECURSION' => [
+                'data' => $recursionData
+            ]
+        ];
+    }
+
+    public function testProvidingArrayThatMixesObjectsAndScalars()
+    {
+        $data = [
+            [123],
+            ['foo'],
+            [$this->createMock(Mockable::class)],
+        ];
+
+        $test = new \TestAutoreferenced('testJsonEncodeException', [$data]);
+        $test->runBare();
+
+        $this->assertInternalType('array', $test->myTestData);
+        $this->assertSame($data, $test->myTestData);
     }
 }
