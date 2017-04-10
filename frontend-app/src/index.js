f639024a19078de16b527def7c9bfe82afdaf457
bmf <bmf.infomation@gmail.com>
2017-04-10 22:50:47 +0900

    [update] require highlight.js in js file

--- frontend-app/src/index.js
+++ frontend-app/src/index.js
@@ -9,6 +9,7 @@ import reducers from './reducers/reducer_root';
 import routes from './routes';
 
 require('../node_modules/bulma/css/bulma.css');
+require('../node_modules/highlight.js/styles/atom-one-dark.css');
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
