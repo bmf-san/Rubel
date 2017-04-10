39de21b8b83224954b780bd9dbe5e861aad32aca
bmf <bmf.infomation@gmail.com>
2017-04-10 21:46:32 +0900

    [fix] api methods

--- backend-app/routes/api.php
+++ backend-app/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,19 +7,19 @@ use Illuminate\Http\Request;
 |--------------------------------------------------------------------------
 */
 
-Route::get('post/{id}', 'Api\v1\Post\Postcontroller@getPost')->where('id', '[0-9]+');;
+Route::get('post/{id}', 'Api\v1\Post\Postcontroller@getPost')->where('id', '[0-9]+');
 Route::get('posts', 'Api\v1\Post\PostController@getPosts');
 Route::get('tags', 'Api\v1\Tag\TagController@getTags');
 Route::get('categories', 'Api\v1\Category\CategoryController@getCategories');
 
 Route::group(['prefix' => 'admin'], function () {  // TODO: Add authentication
-	Route::post('post/create', 'Api\v1\Post\PostController@create');
-	Route::post('post/{id}/edit', 'Api\v1\Post\PostController@edit')->where('id', '[0-9]+');
-	Route::post('post/{id}/update', 'Api\v1\Post\PostController@update')->where('id', '[0-9]+');
-	Route::post('post/{id}/delete', 'Api\v1\Post\PostController@delete')->where('id', '[0-9]+');
-	Route::post('tag/{id}/edit', 'Api\v1\Tag\TagController@edit')->where('id', '[0-9]+');
-	Route::post('tag/{id}/delete', 'Api\v1\Tag\TagController@delete')->where('id', '[0-9]+');
-	Route::post('category/create', 'Api\v1\Category\CategoryController@create');
-	Route::post('category/{id}/edit', 'Api\v1\Category\CategoryController@edit')->where('id', '[0-9]+');
-	Route::post('category/{id}/delete', 'Api\v1\Category\CategoryController@delete')->where('id', '[0-9]+');
+    Route::post('post/create', 'Api\v1\Post\PostController@create');
+    Route::patch('post/{id}/edit', 'Api\v1\Post\PostController@edit')->where('id', '[0-9]+');
+    Route::patch('post/{id}/update', 'Api\v1\Post\PostController@update')->where('id', '[0-9]+');
+    Route::delete('post/{id}/delete', 'Api\v1\Post\PostController@delete')->where('id', '[0-9]+');
+    Route::patch('tag/{id}/edit', 'Api\v1\Tag\TagController@edit')->where('id', '[0-9]+');
+    Route::delete('tag/{id}/delete', 'Api\v1\Tag\TagController@delete')->where('id', '[0-9]+');
+    Route::post('category/create', 'Api\v1\Category\CategoryController@create');
+    Route::patch('category/{id}/edit', 'Api\v1\Category\CategoryController@edit')->where('id', '[0-9]+');
+    Route::delete('category/{id}/delete', 'Api\v1\Category\CategoryController@delete')->where('id', '[0-9]+');
 });
