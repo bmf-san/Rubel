382daccbd91cb34d2b11d0dad59b9a9d2c7c46e1
bmf <bmf.infomation@gmail.com>
2017-04-20 04:04:26 +0900

    [update]

--- backend-app/resources/views/post/show.blade.php
+++ backend-app/resources/views/post/show.blade.php
@@ -5,7 +5,7 @@
 @section('content')
   <div class="container-fluid">
     @include('partials.nav')
-    <section class="hero is-primary is-medium">
+    <section class="hero is-primary is-medium"> <!-- TODO chage header image by post thumb img -->
       <div class="hero-body">
         <div class="container has-text-centered">
           <h1 class="title is-2">
