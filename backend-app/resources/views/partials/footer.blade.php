f4f3d5e513a09793ab33dc659a6e4b725ea45846
bmf <bmf.infomation@gmail.com>
2017-05-05 15:38:51 +0900

    [add] composer.json for packagist

--- backend-app/resources/views/partials/footer.blade.php
+++ backend-app/resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="content has-text-centered">
       <p>
-        <strong>Rubel</strong> by bmf_san. The source code is licensed
+        <strong>Rubel</strong> by bmf. The source code is licensed
         <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
       </p>
       <p>
--- backend-app/resources/views/profile/index.blade.php
+++ backend-app/resources/views/profile/index.blade.php
@@ -9,18 +9,15 @@
       <div class="hero-body">
         <div class="container has-text-centered">
           <h1 class="title is-2">
-            Blog post title
+            Profile
           </h1>
-          <h2 class="subtitle is-4">
-            Blog post subtitle
-          </h2>
         </div>
       </div>
     </section>
     <section class="section">
       <div class="container">
         <div class="column is-8 is-offset-2">
-          Here is profile page.
+          Coming soon ...
         </div>
       </div>
     </section>
