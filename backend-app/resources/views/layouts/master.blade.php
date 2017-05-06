02446fdf1f02eb0f02ade57f7e22dd11d8b611c1
bmf <bmf.infomation@gmail.com>
2017-05-07 04:37:49 +0900

    [update]

--- backend-app/resources/views/layouts/master.blade.php
+++ backend-app/resources/views/layouts/master.blade.php
@@ -2,6 +2,7 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Rubel - @yield('title', 'title')</title>
 </head>
 <body>
