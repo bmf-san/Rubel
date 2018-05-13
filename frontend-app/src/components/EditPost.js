3482b32efbd56e30494718d29c7d2396948213a0
bmf <bmf.infomation@gmail.com>
2018-05-13 18:54:50 +0900

    [update]

--- frontend-app/src/components/EditPost.js
+++ frontend-app/src/components/EditPost.js
@@ -21,7 +21,7 @@ import {Link} from "react-router";
 import Loader from "../utils/Loader";
 
 class EditPost extends Component {
-  constructor() {
+  constructor(props) {
     const {
       params,
       initialize,
